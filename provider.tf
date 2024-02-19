13bfae646a1932da84110db28c6d61835a07da72
Anthunt <anthunt@users.noreply.github.com>
2024-02-19 13:57:32 +0900

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,4 @@
+provider "confluent" {
+  cloud_api_key    = var.confluent_cloud_api_key    # optionally use CONFLUENT_CLOUD_API_KEY env var
+  cloud_api_secret = var.confluent_cloud_api_secret # optionally use CONFLUENT_CLOUD_API_SECRET env var
+}
